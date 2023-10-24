2e1cd7091c4aff22c89c266a5b67798fb491cb0f
Oleg <bombastik@ukr.net>
2023-10-24 15:34:54 +0300

    newstylewidget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { PrimengcomponentsModule } from './shared/primengcomponents/primengcomponents.module';
 import { WidgetComponent } from './components/widget/widget.component';
 import { DragAndDropManagerDirective } from './shared/directives/drag-and-drop-manager.directive';
-import { ZonesComponent } from './components/zones/zones.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { ResizableModule } from 'angular-resizable-element';
 import { ResizibleDirective } from './shared/directives/resizible.directive';
@@ -22,7 +22,6 @@ import { ZoneswithcamsComponent } from './components/zoneswithcams/zoneswithcams
     AppComponent,
     WidgetComponent,
     DragAndDropManagerDirective,
-    ZonesComponent,
     ResizibleDirective,
     MockdatatableComponent, 
     ZoneswithcamsComponent 
